84f8970c7f8661f30916a19fd6c7663e8b6cedc6
Emil Christiansen <emil.christiansen@ntnu.no>
2020-10-29 08:34:04 +0100

    updated GUI source files

---
+++ mib2hspy/gui/source/QTCmib2hspy/mib2hspymainwindow.cpp
@@ -0,0 +1,11 @@
+#include "mib2hspymainwindow.h"
+
+mib2hspyMainWindow::mib2hspyMainWindow(QWidget *parent)
+    : QMainWindow(parent)
+{
+}
+
+mib2hspyMainWindow::~mib2hspyMainWindow()
+{
+}
+
---
+++ mib2hspy/gui/source/QTCmib2hspy/mib2hspymainwindow.h
@@ -0,0 +1,14 @@
+#ifndef MIB2HSPYMAINWINDOW_H
+#define MIB2HSPYMAINWINDOW_H
+
+#include <QMainWindow>
+
+class mib2hspyMainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    mib2hspyMainWindow(QWidget *parent = nullptr);
+    ~mib2hspyMainWindow();
+};
+#endif // MIB2HSPYMAINWINDOW_H
